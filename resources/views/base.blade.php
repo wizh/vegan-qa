2a8ce9b70a80ace68ac79d2bbb9059b3b539377a
wizh <mathias@fleig.io>
2019-07-16 13:21:18 +0200

    Add base view

---
+++ resources/views/base.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+   <meta charset="utf-8">
+   <title>Q & A</title>
+
+   <!-- Bootstrap core CSS -->
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+   <div class="jumbotron text-center">
+     <h1 class="display-4"> <a href="/" style="color:#000">Q & A</a></h1>
+   </div>
+ </head>
+ <body>
+   @yield('content')
+ </body>
+</html>
